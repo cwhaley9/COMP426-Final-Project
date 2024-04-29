ff2fbfdee787dd781fbf807cb6431f11302e16b9
cwhaley9 <cwhaley9@unc.edu>
2024-04-28 20:21:55 -0400

    refactor project

---
+++ src/app/app.routes.ts
@@ -0,0 +1,6 @@
+import { Routes } from '@angular/router';
+import { RegisterAccountComponent } from './register-account/register-account.component';
+
+export const routes: Routes = [{path: 'register', component: RegisterAccountComponent}];
+
+
---
+++ src/app/register-account/register-account.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-register-account',
+  standalone: true,
+  imports: [],
+  templateUrl: './register-account.component.html',
+  styleUrl: './register-account.component.css'
+})
+export class RegisterAccountComponent {
+
+}
