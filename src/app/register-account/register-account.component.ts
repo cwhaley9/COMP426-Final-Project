95519de040cfeddec0e002d593c7cb245dcd9e58
cwhaley9 <cwhaley9@unc.edu>
2024-04-29 19:14:32 -0400

    router-outlet not recognized

--- src/app/register-account/register-account.component.ts
+++ src/app/register-account/register-account.component.ts
@@ -2,8 +2,6 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-register-account',
-  standalone: true,
-  imports: [],
   templateUrl: './register-account.component.html',
   styleUrls: ['./register-account.component.css']
 })
