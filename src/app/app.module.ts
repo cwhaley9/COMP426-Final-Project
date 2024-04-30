7ef41aa8ffcfdb1598a46e210575a42c0ad2024e
cwhaley9 <cwhaley9@unc.edu>
2024-04-29 21:29:01 -0400

    add registration form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { RegisterAccountComponent } from './register-account/register-account.component';
@@ -15,7 +16,8 @@ export const routes: Routes = [
   ],
   imports: [
     CommonModule,
-    RouterModule.forRoot(routes)
+    RouterModule.forRoot(routes),
+    FormsModule
   ],
   bootstrap: []
 })
--- src/app/register-account/register-account.component.ts
+++ src/app/register-account/register-account.component.ts
@@ -8,4 +8,7 @@ import { Component } from '@angular/core';
 })
 export class RegisterAccountComponent {
 
+  onSubmit(){
+    console.log("success");
+  }
 }
